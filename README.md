8b23bf2383131a202e0d229ca6970941910aee0e
张伟华 <gzlingye@gmail.com>
2019-04-05 00:14:55 +0800

    1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # nightmare-precookies
+
+## Usage
+Require the library and pass the Nightmare library as a reference to attach the plugin actions:
+
+```js
+var Nightmare = require('nightmare');
+require('nightmare-precookies')(Nightmare);
+```
+
+### .preCookies(cookies)
+
+## Example
+```js
+await nightmare.preCookies(`myCookie:hi`).goto(`https://www.github.com`)
+```
---
+++ nightmare-precookies.js
@@ -0,0 +1,28 @@
+var debug = require('debug')('nightmare:preCookies');
+
+module.exports = exports = function(Nightmare) {
+  Nightmare.action(
+    'preCookies',
+    (name, options, parent, win, renderer, done) => {
+      parent.on('preCookies', (cookies) => {
+        win.webContents.session.webRequest.onBeforeSendHeaders((details, cb) => {
+          if (!details) return;
+          var headers = details.requestHeaders;
+          if (headers.Cookie == null || headers.Cookie == '') {
+            headers.Cookie = cookies;
+            cb({ cancel: false, requestHeaders: headers })
+          } else {
+            cb({ cancel: false })
+          }
+        })
+        parent.emit('preCookies');
+      })
+      done()
+      return this;
+    },
+    function(cookies, done) {
+      this.child.once('preCookies', done);
+      this.child.emit('preCookies', cookies);
+    }
+  )
+};
